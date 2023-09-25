10ee6bf3d3fc9c75ebf94f36dd0ff1fddbab82a2
Himanshu Soni <himanshusoni@jmangroup.com>
2023-09-25 18:43:14 +0530

    .

---
+++ backend/src/model/job.ts
@@ -0,0 +1,59 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../database";
+
+class Job extends Model {
+  public id!: number;
+  public title!: string;
+  public description!: string;
+  public company!: string;
+  public location!: string;
+  public salary!: number;
+  public postedAt!: Date;
+  public postedBy!: string;
+}
+
+Job.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    company: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    location: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    salary: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    postedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    postedBy: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "users",
+  }
+);
+
+export { Job };
